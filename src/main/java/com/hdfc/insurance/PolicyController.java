2eb1957d2bb2ae7f2617b223f9bfafd7ab82e273
devopsmvc <devops.mvc@gmail.com>
2022-12-16 16:04:41 +0530

    01. Created Insurance Project

---
+++ src/main/java/com/hdfc/insurance/PolicyController.java
@@ -0,0 +1,53 @@
+package com.hdfc.insurance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PolicyController {
+	
+	@Autowired
+	PolicyService PolicyService;
+	
+	@GetMapping("/helloworld")
+	public String helloWorld() {  
+		return "Hello World"; 
+	}
+	 
+	@GetMapping("/sayhi")
+	public String sayhi() {
+		return "Hi from Spring Boot";
+	}
+	
+	
+	@PostMapping("/CreatePolicy")
+	public Policy CreatePolicy() {
+		
+		return PolicyService.CreatePolicy();
+	}
+	
+	@PutMapping("/UpdatePolicy")
+	public Policy UpdatePolicy(@RequestBody Policy Policy) {
+		
+		return PolicyService.UpdatePolicy(Policy);
+	}
+
+	
+	@GetMapping("/ViewPolicy/{PolicyID}")
+	public Policy ViewPolicyDetails(@PathVariable(value="PolicyID") int PolicyID) {
+		return PolicyService.ViewPolicy(PolicyID);
+	}
+	
+	@DeleteMapping("/DeletePolicy/{PolicyID}")
+	public void DeletePolicyDetails(@PathVariable(value="PolicyID") int PolicyID) {
+		PolicyService.DeletePolicy(PolicyID);
+	}
+
+
+}
---
+++ src/main/java/com/hdfc/insurance/PolicyService.java
@@ -0,0 +1,32 @@
+package com.hdfc.insurance;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyService {
+	
+	@Autowired
+	PolicyRepository PolicyRepository;
+	
+	public Policy CreatePolicy() {
+		Policy e = new Policy(12345, "Mahesh", 98765);
+		return e;
+		
+	}
+	
+	public Policy UpdatePolicy(Policy Policy) {
+		return PolicyRepository.save(Policy);
+	}
+
+		
+
+	public Policy ViewPolicy(int PolicyID) {
+		return PolicyRepository.findById(PolicyID).get();
+	}
+	
+	public void  DeletePolicy(int PolicyID) {
+		PolicyRepository.deleteById(PolicyID);
+	}
+
+}
