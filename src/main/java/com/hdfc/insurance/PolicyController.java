1adaaf8d904b0b2fbe1ee36f34384c4baeb563f5
devopsmvc <devops.mvc@gmail.com>
2022-12-16 19:19:22 +0530

    Jenkinsfile added

--- src/main/java/com/hdfc/insurance/PolicyController.java
+++ src/main/java/com/hdfc/insurance/PolicyController.java
@@ -1,12 +1,14 @@
 package com.hdfc.insurance;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +16,12 @@ public class PolicyController {
 	
 	@Autowired
 	PolicyService PolicyService;
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
 	
 	@GetMapping("/helloworld")
 	public String helloWorld() {  
